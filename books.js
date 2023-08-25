7129a42ab80f12d7a65ad63582f17b01b1bfbc4e
ShelbyEliza <shelbyelizashipley@gmail.com>
2023-08-25 15:00:49 -0700

    Add CSS styles. Delete, add, and modal function

--- books.js
+++ books.js
@@ -1,30 +1,13 @@
-const addBtn = document.querySelector(".add");
-const dialogEl = document.querySelector(".modal");
-const cancelBtn = document.querySelector(".cancel");
-const submitBtn = document.querySelector(".submit");
-const inputEls = document.querySelectorAll(".input");
-const shelfEl = document.querySelector(".shelf");
-
-addBtn.addEventListener("click", (e) => {
-	console.log(e);
-	dialogEl.showModal();
-});
-// cancelBtn.addEventListener("click", dialogEl.close());
-submitBtn.addEventListener("click", (e) => {
-	handleSubmit(e);
-});
-
+/** Array to store Data: */
 const myLibrary = [];
+
+/** Dummy Data: */
 let theHobbit = ["The Hobbit", "J.R.R. Tolkien", 295, "on"];
-let theFellowship = [
-	"The Fellowship of the Ring",
-	"J.R.R. Tolkien",
-	408,
-	"off",
-];
+let theFellowship = ["Women Talking", "Miriam Toews", 408, "on"];
 let kindred = ["Kindred", "Octavia Butler", 242, "on"];
 let persuasion = ["Persuasion", "Jane Austen", 280, "on"];
 
+/** Book Object: */
 function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
@@ -41,46 +24,90 @@ function Book(title, author, pages, read) {
 
 Book.prototype.getRandomID = function () {
 	let randomNumb = Math.floor(Math.random() * (500 - 1 + 1)) + 1;
-	return this.author[0] + "-" + randomNumb;
+	return this.title[0] + this.author[0] + "-" + randomNumb;
 };
+
+/** Select HTML Elements: */
+const addNewBtn = document.querySelector(".add");
+const dialogEl = document.querySelector(".modal");
+const cancelBtn = document.querySelector(".cancel");
+const submitBtn = document.querySelector(".submit");
+const inputEls = document.querySelectorAll(".input");
+const shelfEl = document.querySelector(".shelf");
+const formEl = document.querySelector("#form");
+const invalidEl = document.querySelector(".invalid-input");
+
+/** Add Event Listeners: */
+addNewBtn.addEventListener("click", (e) => {
+	dialogEl.showModal();
+});
+
+submitBtn.addEventListener("click", (e) => {
+	handleSubmit(e);
+});
+
+cancelBtn.addEventListener("click", () => {
+	invalidEl.textContent = "";
+
+	dialogEl.close();
+});
+
+/** Add Dummy Data to myLibrary Array: */
+addBookToLibrary(theHobbit);
+addBookToLibrary(theFellowship);
+addBookToLibrary(kindred);
+addBookToLibrary(persuasion);
+myLibrary.forEach((book) => buildCard(book));
 
 function addBookToLibrary(book) {
 	let newBook = new Book(...book);
 
 	myLibrary.push(newBook);
+
 	return newBook;
 }
-
-addBookToLibrary(theHobbit);
-// addBookToLibrary(theFellowship);
-// addBookToLibrary(kindred);
-// addBookToLibrary(persuasion);
-
-console.log(myLibrary);
 
 function handleSubmit(e) {
 	e.preventDefault();
 	let newBook = [];
+	let validForm = true;
+
 	inputEls.forEach((el) => {
-		newBook.push(el.value);
+		if (el.type !== "checkbox" && validForm === true) {
+			/** Check if inputs have content: */
+			if (el.value.length >= 1) {
+				newBook.push(el.value);
+			} else {
+				invalidEl.textContent = "Please enter valid input!";
+				validForm = false;
+				return;
+			}
+		} else if (el.type === "checkbox" && validForm === true) {
+			newBook.push(el.checked);
+		}
 	});
-	console.log(newBook);
-	let bookObj = addBookToLibrary(newBook);
-	buildCard(bookObj);
-	dialogEl.close();
+
+	if (validForm === true) {
+		invalidEl.textContent = "";
+
+		let bookObj = addBookToLibrary(newBook);
+		buildCard(bookObj);
+		dialogEl.close();
+	} else {
+		return;
+	}
 }
 
 function handleDelete(e, bookToDelete) {
-	// console.log(bookToDelete.id);
-	myLibrary.filter((book) => {
-		let res;
-		if (book.id !== bookToDelete.id) {
-			res = book;
-		}
-		return res;
-	});
-	// myLibrary.filter((book) => book.id != bookToDelete.id);
-	console.log(myLibrary);
+	const cardsArray = [...document.querySelectorAll(".card")];
+
+	let elToDelete = cardsArray.find((el) => el.id === bookToDelete.id);
+	let indexToDelete = myLibrary.findIndex(
+		(book) => book.id === bookToDelete.id
+	);
+
+	elToDelete.remove();
+	myLibrary.splice(indexToDelete, 1);
 }
 
 function buildCard(book) {
@@ -93,14 +120,25 @@ function buildCard(book) {
 	let readP = document.createElement("p");
 
 	cardDiv.classList.add("card");
+	byP.classList.add("author-line", "by");
+	authorP.classList.add("author-line", "author-name");
+	pagesP.classList.add("extra-info", "pages");
+	readP.classList.add("extra-info", "read-bool");
+
 	cardDiv.dataset.id = book.id;
+	cardDiv.id = book.id;
 
 	deleteBtn.textContent = "X";
 	titleH3.textContent = book.title;
 	byP.textContent = "by";
 	authorP.textContent = book.author;
 	pagesP.textContent = book.pages + " pages";
-	readP.textContent = book.read;
+
+	if (book.read === true) {
+		readP.textContent = "Read";
+	} else {
+		readP.textContent = "Not Read";
+	}
 
 	cardDiv.appendChild(deleteBtn);
 	cardDiv.appendChild(titleH3);
@@ -115,3 +153,5 @@ function buildCard(book) {
 		handleDelete(e, book);
 	});
 }
+
+console.log(myLibrary);
